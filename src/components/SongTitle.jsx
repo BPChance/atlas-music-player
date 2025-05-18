634ff4e16892e25cb91a79af3fa000372d3cfd91
BPChance <bradenpchance6@gmail.com>
2025-05-18 17:52:23 -0500

    music player components

---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,8 @@
+export default function SongTitle() {
+  return (
+    <div className="flex flex-col">
+      <h2 className="text-2xl font-bold text-blue-500">Tidal Drift</h2>
+      <h4 className="text-base">Echoes of the Sea</h4>
+    </div>
+  );
+}
