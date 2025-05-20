762bb6eab221fbfa287cbc1697a275d52a69d901
BPChance <bradenpchance6@gmail.com>
2025-05-20 00:08:31 -0500

    idk whats going on nothing works, awesome

--- src/components/PlayListItem.jsx
+++ src/components/PlayListItem.jsx
@@ -1,9 +1,11 @@
 export default function PlayListItem() {
   return (
-    <div>
-      <p>song title</p>
-      <p>artist</p>
-      <p>length</p>
+    <div className="flex items-center justify-between">
+      <div className="flex flex-col">
+        <p>Song Title</p>
+        <p>Artist</p>
+      </div>
+      <p>3:42</p>
     </div>
   );
 }
